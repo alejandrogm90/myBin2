b9eb90516b2322f044a17796ce3012f30bff76ac
alejandrogm90 <alejandrogomezmartin90@gmail.com>
2022-11-16 20:37:23 +0100

    * update 2022-11-16

--- updateAll.sh
+++ updateAll.sh
@@ -2,7 +2,8 @@
 
 git submodule sync
 git submodule update --init --recursive
-git pull --recurse-submodules
+git submodule update --remote --merge
+
 
 # VARIABLES Y FUNCONES
 DIR_HOME=$(cd `dirname $0` && pwd)
@@ -22,21 +23,6 @@ export script_info=(
 
 showScriptInfo
 
-function updateRepo {
-    cd "$1"
-    git pull "$2" main
-    cd "${DIR_HOME}"
-}
-
-# Update
-updateRepo "${REPO_1}" "https://github.com/alejandrogm90/python3_examples"
-updateRepo "${REPO_2}" "https://github.com/alejandrogm90/ssh_examples"
-
-if [ $# -eq 1 ] && [ "$1" == "clean" ] ; then
-    rm ${MYBIN2_LOCATION}/*
-    echo "" > ${MYBIN2_LOCATION}/.gitkeep 
-fi
-
 # Copy all files
 echo "Copying SSH examples"
 cp -f "${DIR_HOME}/ssh_examples/scripts/"*.sh "${MYBIN2_LOCATION}/"
@@ -45,4 +31,3 @@ cp -f "${DIR_HOME}/python3_examples/"*.py "${MYBIN2_LOCATION}/"
 
 # Add execution permissions
 chmod +x ${MYBIN2_LOCATION}/*
-
