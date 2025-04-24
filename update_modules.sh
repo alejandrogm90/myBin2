742b9eae9ab4e9445a6ca87b57dd6eec2084444e
Alejandro Gómez Martín <alejandrogomezmartin90@gmail.com>
2025-04-24 10:56:14 +0200

    * Reformat sh for updates

--- update_modules.sh
+++ update_modules.sh
@@ -4,11 +4,11 @@
 git submodule update --recursive --init
 git submodule update --recursive --remote
 
-# Varables and functions
-DIR_HOME=$(cd `dirname $0` && pwd)
+# Variables and functions
+DIR_HOME=$(cd "$(dirname "$0")" && pwd)
 source "${DIR_HOME}/sh_examples/scripts/commonFunctions.sh"
-SCRIPT_NAME=`getJustStriptName $0`
-MYBIN2_LOCATION="${DIR_HOME}/bin"
+SCRIPT_NAME=$(getJustStriptName "$0")
+MY_BIN2_LOCATION="${DIR_HOME}/bin"
 
 # Show basic script info
 declare -A script_info
@@ -23,17 +23,17 @@ showScriptInfo
 # Cleaning previous data
 if [ $# -eq 1 ] && [ "$1" == "-c" ] ; then
     echo "Cleaning previous data"
-    rm -f "${MYBIN2_LOCATION}/"*.sh
-    rm -f "${MYBIN2_LOCATION}/"*.py
+    rm -f "${MY_BIN2_LOCATION}/"*.sh
+    rm -f "${MY_BIN2_LOCATION}/"*.py
 fi
 
 # Copy all files
 echo "Copying SSH examples"
-cp -f "${DIR_HOME}/sh_examples/scripts/"*.sh "${MYBIN2_LOCATION}/"
+cp -f "${DIR_HOME}/sh_examples/scripts/"*.sh "${MY_BIN2_LOCATION}/"
 echo "Copying Python3 examples"
-cp -f "${DIR_HOME}/python3_examples/src/"*.py "${MYBIN2_LOCATION}/"
-cp -f "${DIR_HOME}/python3_examples/"*.conf "${MYBIN2_LOCATION}/"
+cp -f "${DIR_HOME}/python3_examples/src/"*.py "${MY_BIN2_LOCATION}/"
+cp -f "${DIR_HOME}/python3_examples/"*.conf "${MY_BIN2_LOCATION}/"
 
 # Add execution permissions
-chmod +x "${MYBIN2_LOCATION}/"*.sh
-chmod +x "${MYBIN2_LOCATION}/"*.py
+chmod +x "${MY_BIN2_LOCATION}/"*.sh
+chmod +x "${MY_BIN2_LOCATION}/"*.py
